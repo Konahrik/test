d56724525f8d5aca13a308ee242ed053d730360f
drogova <rogova.daria.u@gmail.com>
2016-01-13 13:29:53 +0300

    length

--- ConsoleApplication2/Product.cs
+++ ConsoleApplication2/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication2
@@ -10,6 +11,7 @@ namespace ConsoleApplication2
     {
         public int ID { get; set; }
         public string ProductName { get; set; }
+        [MaxLength(1)]
         public int Rate { get; set; }
         public double Price { get; set; }
     }
